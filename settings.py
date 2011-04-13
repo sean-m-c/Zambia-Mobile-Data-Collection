4897520d9b535c2908c9f30864d3e4d076ecc3fb
Sean Clark <smclark89@gmail.com>
2011-04-13 18:17:19 -0400

    Updated requirements & .gitignore

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ MEDIA_ROOT = '/users/sclark/project/Project/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = 'http://127.0.0.1:8000/Project/media/'
+MEDIA_URL = 'http://127.0.0.1:8888/Project/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -31,4 +31,4 @@ STATIC_ROOT = '/users/sclark/project/Project/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://127.0.0.1:8000/Project/static/'
+STATIC_URL = 'http://127.0.0.1:8888/Project/static/'
--- settings_default.py
+++ settings_default.py
@@ -59,7 +59,7 @@ MEDIA_URL = 'http://184.72.227.76/Project/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/opt/bitnami/projects/Project/static/'
+STATIC_ROOT = '/opt/bitnami/projects/Project/static/'   
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
